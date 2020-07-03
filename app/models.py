3711867db782c9d7d4d4db85ffdb289f68a83323
Sachidanand Alle <salle@nvidia.com>
2020-07-02 20:08:26 -0700

    clara dashboard template

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
-# -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
-from django.contrib.auth.models import User
 
-# Create your models here.
 
+class MMAR(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    ngc_path = models.CharField(max_length=255)
+    version = models.IntegerField(default=1)
+    downloaded_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+    status = models.CharField(max_length=255, null=True)
+
+class Dataset(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    path = models.CharField(max_length=255)
+    is_gdrive = models.BooleanField(default=False)
+    downloaded_at = models.DateTimeField(null=True)
+    description = models.CharField(max_length=255, null=True)
+    status = models.CharField(max_length=255, null=True)
--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,10 @@
-# -*- encoding: utf-8 -*-
-"""
-License: MIT
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
 
+
 # Create your models here.
+class MMAR(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    ngc_path = models.CharField(max_length=255)
+    version = models.IntegerField(default=1)
+    downloaded_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
